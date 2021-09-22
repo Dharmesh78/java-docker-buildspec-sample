c07fdf797d8daf370cfa3898c614fd4821ea3618
Dharmesh Singh <36789384+Dharmesh78@users.noreply.github.com>
2021-09-22 19:59:11 +0530

    Update compareValues.sh

--- compareValues.sh
+++ compareValues.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [ $OCI_STAGE_ID == ]
+if [ $OCI_STAGE_ID ==  
+"ocid1.devopsbuildpipelinestage.oc1.iad.amaaaaaa4725fbqa5kv7zzw7l5ksrovxcsytjb4tuzbb2ujyb4jngq4m5nbq"]
 then
-   echo "OCI_STAGE_ID True" >> output.txt
+   echo "Build stage OCI_STAGE_ID True" >> output.txt
 else
-   echo "OCI_STAGE_ID False" >> output.txt
+   echo "Build stage OCI_STAGE_ID False" >> output.txt
 fi
