a8335e0b24922c74a85c1f9c7ef32a9f14e32dbf
Dharmesh Singh <36789384+Dharmesh78@users.noreply.github.com>
2021-09-22 20:36:45 +0530

    Update compareValues.sh

--- compareValues.sh
+++ compareValues.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
-echo "Build stage OCI_STAGE_ID True" >> output.txt
+echo "----Build stage OCI_STAGE_ID-----" >> output.txt
 echo $OCI_STAGE_ID >> output.txt
 
-echo " OCI_PIPELINE_ID True" >> output.txt
+echo "-----OCI_PIPELINE_ID-----" >> output.txt
 echo $OCI_PIPELINE_ID >> output.txt
 
-echo "Build run id" >> output.txt
+echo "------Build run id-----" >> output.txt
 echo $OCI_BUILD_RUN_ID >> output.txt
 
-echo "source branch name" >> output.txt
+echo "----trigger source branch name---" >> output.txt
 echo $OCI_TRIGGER_SOURCE_BRANCH_NAME>> output.txt
 
 
-echo "source commit hash" >> output.txt
+echo "---trigger source commit hash---" >> output.txt
 echo $OCI_TRIGGER_COMMIT_HASH >> output.txt
 
-echo "source commit hash" >> output.txt
+echo "----trigger source commit hash---" >> output.txt
 echo $OCI_TRIGGER_COMMIT_HASH >> output.txt
 
-echo "source url" >> output.txt
+echo "---trigger source url---" >> output.txt
 echo $OCI_TRIGGER_SOURCE_URL >> output.txt
 
-echo "source directory" >> output.txt
+echo "----source directory----" >> output.txt
 echo $OCI_PRIMARY_SOURCE_DIR >> output.txt
 
-echo "workspace directory" >> output.txt
+echo "----workspace directory-------" >> output.txt
 echo $OCI_WORKSPACE_DIR >> output.txt
 
 
-echo "build stage name" >> output.txt
+echo "-----build stage name---------" >> output.txt
 echo $OCI_BUILD_STAGE_NAME >> output.txt
 
-echo "workspace buildspec.yaml path" >> output.txt
+echo "-----workspace buildspec.yaml path-------" >> output.txt
 echo ${OCI_WORKSPACE_DIR}/buildSpec.yml >> output.txt
 
 echo "primary source name" >> output.txt
