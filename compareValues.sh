c1029e95f4b292af0e7599aa58fb9a9ff5602ad4
Dharmesh Singh <36789384+Dharmesh78@users.noreply.github.com>
2021-09-22 20:07:03 +0530

    Update compareValues.sh

--- compareValues.sh
+++ compareValues.sh
@@ -6,3 +6,17 @@ then
 else
    echo "Build stage OCI_STAGE_ID False" >> output.txt
 fi
+
+if [ $OCI_PIPELINE_ID ==  
+"oocid1.devopsbuildpipeline.oc1.iad.amaaaaaa4725fbqadtrcrxopo3vqqao4awo4ee5o6imjje327qklizxzcuda"]
+then
+   echo " OCI_PIPELINE_ID True" >> output.txt
+else
+   echo " OCI_PIEPELINE_ID False" >> output.txt
+fi
+
+echo "Build run id"
+echo $OCI_BUILD_RUN_ID >> output.txt
+
+
+
